d04035fdf7ef687ec1c74fe1f5894002cd612cd8
oldchili <130549691+oldchili@users.noreply.github.com>
2025-09-17 13:25:32 +0300

    Update src/L1GovernanceRelay.sol
    Co-authored-by: telome <130504305+telome@users.noreply.github.com>

--- src/L1GovernanceRelay.sol
+++ src/L1GovernanceRelay.sol
@@ -91,7 +91,7 @@ contract L1GovernanceRelay {
     // It is not likely that lzTokenFee is used, support is added here just for completeness.
     // In case it is used, governance is assumed to monitor LZ for token changes.
     // If deemed needed, this includes a check in the spell itself and fallback code.
-    // Also assuming that the send library is configured implicitly, so any default behavior is not relied on (as can change).
+    // Also assuming that the send library is configured explicitly, so any default behavior is not relied on (as can change).
     // Also assuming that if authed to multiple senders, they are trusted not to steal/waste eth/tokens from each other.
     // (dstEid, l2GovernanceRelay) is assumed to be whitelisted in the l1 Oapp for this src sender.
     function relayEVM(
