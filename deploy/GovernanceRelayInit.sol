bcb1f0388f452cafd738e2e6d4ecfb6b7f684dca
oldchili <130549691+oldchili@users.noreply.github.com>
2025-09-17 12:39:20 +0300

    Minor changes

--- deploy/GovernanceRelayInit.sol
+++ deploy/GovernanceRelayInit.sol
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2024 Dai Foundation <www.daifoundation.org>
+// SPDX-FileCopyrightText: © 2025 Dai Foundation <www.daifoundation.org>
 // SPDX-License-Identifier: AGPL-3.0-or-later
 //
 // This program is free software: you can redistribute it and/or modify
@@ -23,6 +23,8 @@ interface L1GovernanceRelayLike {
 }
 
 library GovernanceRelayInit {
+
+    // We assume no lzToken support on launch.
     function init(DssInstance memory dss, address l1GovernanceRelay, address l1Oapp) internal {
         L1GovernanceRelayLike(l1GovernanceRelay).file("l1Oapp", l1Oapp);
         dss.chainlog.setAddress("LZ_GOV_RELAY", l1GovernanceRelay);
